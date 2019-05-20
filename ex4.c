22f99795fd5c15e7020a9e13b6396f145c8dcadb
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2019-05-20 20:26:52 +0300

    Add files via upload

---
+++ ex4.c
@@ -0,0 +1,46 @@
+#include <avr/io.h>
+char x;
+int main(void)
+{
+	DDRA=0xFF;
+	DDRC=0x00;
+// Αρχικοποίηση PORTB ως output
+// Αρχικοποίηση PORTA ως input
+	x = 1;
+// Αρχικοποίηση μεταβλητής για αρχικά αναμμένο led
+	while(1)
+	{
+		if ((PINC & 0x01)){
+
+			while ((PINC & 0x01)); 
+			if (x==128)
+				x = 1;
+			else
+				x = x<<1; 
+		}
+		else if ((PINC & 0x02)){
+
+			while ((PINC & 0x02)); 
+			if (x==1)
+				x = 128;
+			else
+				x = x>>1; 
+		}
+		else if ((PINC & 0x04)){
+
+			while ((PINC & 0x01)); 
+			
+				x = 128;
+			
+		}
+		else if ((PINC & 0x08)){
+
+			while ((PINC & 0x08)); 
+			
+				x = 1;
+
+		}
+		PORTA = x;
+	}
+	return 0;
+}
