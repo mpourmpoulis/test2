249544d441c02a37bd6ea32c3087d8cba2cdca41
Kitsios Panagiotis <35875229+mpourmpoulis@users.noreply.github.com>
2020-06-01 16:25:30 +0300

    Create arch.sh

---
+++ arch.sh
@@ -0,0 +1,12 @@
+$ wget http://www.cslab.ece.ntua.gr/courses/advcomparch/files/askiseis/sniper7.3.tgz
+$ tar xvfz sniper-7.3.tgz
+$ cd sniper-7.3
+
+$wget
+http://software.intel.com/content/dam/develop/external/us/en/protected/pinplaydcfg-3.11-pin-3.11-97998-g7ecce2dac-gcc-linux.tar.bz2
+$ tar xvfj pinplay-dcfg-3.11-pin-3.11-97998-g7ecce2dac-gcc-linux.tar.bz2
+sudo apt-get update
+$ sudo apt-get install python make g++ zlib1g-dev libbz2-dev libboost-dev
+libsqlite3-dev
+$ export PIN_HOME=/path/to/pinplay-dcfg-3.11-pin-3.11-97998-g7ecce2dac-gcc-linux
+$ make
